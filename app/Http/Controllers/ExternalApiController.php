6ae2dd9ed6e97bcf6a731e0a22715dfcb4540116
Cristian Molina <cristianmolinab121@gmail.com>
2025-03-14 17:09:12 -0500

    comit cristian

--- app/Http/Controllers/ExternalApiController.php
+++ app/Http/Controllers/ExternalApiController.php
@@ -20,4 +20,16 @@ class ExternalApiController extends Controller
     }
 
 
+    public function getUser() {
+
+        $response= Http::get('http://127.0.0.1:8082/api/userservice'); //Api consumida de pablo
+
+
+        //Toma la respuesta del api y la retorna para mostrarla
+        return response()->json([
+            'status' => 'success',
+            'data' => $response->json()
+        ], 200);
+    }
+
 }
